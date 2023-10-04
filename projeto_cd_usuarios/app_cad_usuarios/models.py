50ad1eb22e063f22ec2fa2d12ba51a87651ce161
Gpontes15 <gpontes203@gmail.com>
2023-10-04 14:21:36 -0300

    refactor: alter home.html views.py

--- projeto_cd_usuarios/app_cad_usuarios/models.py
+++ projeto_cd_usuarios/app_cad_usuarios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=255)
+    nome = models.CharField(max_length=255)
     idade = models.IntegerField()
     cpf = models.CharField(max_length=11)
 
