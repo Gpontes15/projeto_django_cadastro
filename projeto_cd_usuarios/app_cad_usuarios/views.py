2a25bbdfa5f2a88ac8df5affd325d364ba002f6d
Gpontes15 <gpontes203@gmail.com>
2023-10-04 14:23:27 -0300

    altered

--- projeto_cd_usuarios/app_cad_usuarios/views.py
+++ projeto_cd_usuarios/app_cad_usuarios/views.py
@@ -8,18 +8,22 @@ def admini(request):
     return render(request,"usuarios/admini.html")
 
 def usuarios(request):
-    # Salvar os dados da tela para o banco de dados.
-    novo_usuario = Usuario()
-    novo_usuario.nome = request.POST.get('nome')
-    novo_usuario.idade = request.POST.get('idade')
-    novo_usuario.cpf = request.POST.get('cpf')
-    novo_usuario.save()
-
-    # Exibir todos os usuarios jpa cadastrados em uma nova página
+    action = request.POST.get('action') # Obtém o valor do botão clicado
     usuarios = {
-        'usuarios': Usuario.objects.all()
-    }
-    
-    # Retornar os dados para a página de listagem de usuarios
-    return render(request,"usuarios/usuarios.html",usuarios)
+                'usuarios': Usuario.objects.all()
+            }
+    if request.method == 'POST':
+        if action == 'Enviar':
+            novo_usuario = Usuario()
+            novo_usuario.nome = request.POST.get('nome')
+            novo_usuario.idade = request.POST.get('idade')
+            novo_usuario.cpf = request.POST.get('cpf')
+            novo_usuario.save()
 
+            # Exibir todos os usuarios já cadastrados em uma nova página
+            
+            
+            # Retornar os dados para a página de listagem de usuarios
+            return render(request,"usuarios/usuarios.html",usuarios)
+        
+    return render(request,"usuarios/usuarios.html",usuarios)
